c8b0ae5bcac1c33e869a3bf6c708dc2892fe73d3
Erkan-5Minds <erkan.cetin@5minds.de>
2022-09-22 13:17:26 +0200

    Finish Week_5

--- Week_5/speller/dictionary.c
+++ Week_5/speller/dictionary.c
@@ -1,7 +1,13 @@
 // Implements a dictionary's functionality
-
-#include <ctype.h>
+#include <string.h>
 #include <stdbool.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+
+
+unsigned int Count_size = 0;
 
 #include "dictionary.h"
 
@@ -13,43 +19,93 @@ typedef struct node
 }
 node;
 
-// TODO: Choose number of buckets in hash table
-const unsigned int N = 26;
+// Number of buckets in hash table
+const unsigned int N = 500;
 
 // Hash table
 node *table[N];
 
-// Returns true if word is in dictionary, else false
+// Returns true if word is in dictionary else false
 bool check(const char *word)
 {
-    // TODO
+    int index = hash(word);
+
+    node *cursor = table[index];
+    
+    while (cursor != NULL)
+    {
+        if(strcasecmp(cursor -> word, word) == 0)
+        {
+            return true;
+        }
+        cursor = cursor -> next;
+    }
     return false;
 }
 
 // Hashes word to a number
 unsigned int hash(const char *word)
 {
-    // TODO: Improve this hash function
-    return toupper(word[0]) - 'A';
+    unsigned int value = 0;
+    unsigned int key_len = strlen(word);
+    for (int i = 0; i < key_len; i++)
+    {
+        value = value + 37 * tolower(word[i]);
+    }
+    value = value % N;
+    return value;
 }
 
-// Loads dictionary into memory, returning true if successful, else false
+// Loads dictionary into memory, returning true if successful else false
 bool load(const char *dictionary)
 {
-    // TODO
-    return false;
+    FILE *open_dictionary = fopen(dictionary,"r");
+    if (open_dictionary == NULL)
+    {
+        return false;
+    }
+    char Dword[LENGTH + 1];
+    while(fscanf(open_dictionary,"%s", Dword) != EOF)
+    {
+        node *newNode = malloc(sizeof(node));
+        if (newNode == NULL)
+        {
+            return false;
+        }
+        strcpy(newNode -> word, Dword);
+        newNode -> next = NULL;
+        int index = hash(Dword);
+
+        if (table[index] == NULL)
+        {
+            table[index] = newNode;
+        }
+        else
+        {
+            newNode -> next = table[index];
+            table[index] = newNode;
+        }
+        Count_size++;
+    }
+    fclose(open_dictionary);
+    return true;
 }
 
-// Returns number of words in dictionary if loaded, else 0 if not yet loaded
+// Returns number of words in dictionary if loaded else 0 if not yet loaded
 unsigned int size(void)
 {
-    // TODO
-    return 0;
+    return Count_size;
 }
 
-// Unloads dictionary from memory, returning true if successful, else false
+// Unloads dictionary from memory, returning true if successful else false
 bool unload(void)
 {
-    // TODO
-    return false;
+    for (int i = 0; i < N; i++)
+    while (table[i] != NULL)
+    {
+        node *tmp = table[i] ->next;
+        free(table[i]);
+        table[i] = tmp;
+    }
+    return true;
 }
