30840783ba0c50a0e508f8a1968ff0d437d16332
Erkan-5Minds <erkan.cetin@5minds.de>
2022-09-29 14:00:39 +0200

    Finish Tournement

--- Week_6/world-cup/tournament.py
+++ Week_6/world-cup/tournament.py
@@ -15,10 +15,23 @@ def main():
         sys.exit("Usage: python tournament.py FILENAME")
 
     teams = []
+    filename = sys.argv[1]
     # TODO: Read teams into memory from file
+    with open(filename) as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            row["rating"] = int(row["rating"])
+            teams.append(row)
+            #   print(row)
 
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
+    for i in range(N):
+        winner = simulate_tournament(teams)
+        if winner in counts:
+            counts[winner] += 1
+        else:
+            counts[winner] = 1
 
     # Print each team's chances of winning, according to simulation
     for team in sorted(counts, key=lambda team: counts[team], reverse=True):
@@ -49,8 +62,11 @@ def simulate_round(teams):
 
 def simulate_tournament(teams):
     """Simulate a tournament. Return name of winning team."""
-    # TODO
+    while len(teams) > 1:
+        teams = simulate_round(teams)
+    return teams[0]["team"]
 
 
 if __name__ == "__main__":
     main()
+    
