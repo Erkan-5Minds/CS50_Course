bbd9e0d7f427e26409b6ae45b64ff695327dca64
Erkan-5Minds <erkan.cetin@5minds.de>
2022-09-29 15:33:07 +0200

    Finish Mario-Less

--- Week_6/sentimental-mario-less/mario.py
+++ Week_6/sentimental-mario-less/mario.py
@@ -1,1 +1,21 @@
 # TODO
+def main():
+    while True:
+        try:
+            height = int(input("Height: "))
+            if (height >= 1) and (height <= 8):
+                break
+        except:
+            print("", end="")
+
+    spaces = 1
+    for j in range(height):
+        for spaces in range(height - j - 1):
+            print(" ", end="")
+
+        for i in range(j + 1):
+            print("#", end="")
+
+        print()
+if __name__ == "__main__":
+    main()
