018f6a0dc52ee3257292f1e4977df280863436b4
Erkan-5Minds <erkan.cetin@5minds.de>
2022-09-30 13:57:54 +0200

    Finish sentimental-cash.py

--- Week_6/sentimental-cash/cash.py
+++ Week_6/sentimental-cash/cash.py
@@ -1,1 +1,28 @@
 # TODO
+def main():
+    while True:
+         change = input("How much change is owned: ")
+         if change < 0:
+            break
+    convertCents = round(change * 100)
+    coins = 0
+
+    while convertCents >= 25:
+        convertCents = convertCents - 25
+        coins += 1
+    
+    while convertCents >= 10:
+        convertCents = convertCents - 10
+        coins += 1
+    
+    while convertCents >= 5:
+        convertCents = convertCents - 5
+        coins += 1
+    
+    while convertCents >= 1 == convertCents > 0:
+        convertCents = convertCents - 1
+        coins += 1
+    
+    print(f"%d\n", coins)
+    
+    main()
