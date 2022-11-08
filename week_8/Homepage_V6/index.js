f8bc709a6a8da41e5764d2170d49852b276f2373
Erkan-5Minds <erkan.cetin@5minds.de>
2022-11-08 16:51:50 +0100

    fxing styling issues

--- week_8/Homepage_V6/index.js
+++ week_8/Homepage_V6/index.js
@@ -1,54 +1,54 @@
-menu1_is_open = false;
-menu2_is_open = false;
-menu3_is_open = false;
+menu1IsOpen  = false;
+menu2IsOpen  = false;
+menu3IsOpen  = false;
 
-function open_on_click(number) {
+function openOnClick(number) {
   num = number;
   const arrow1 = document.getElementById("projekt_one_arrow");
   const projektOneContainer = document.getElementById("projekt_one_container");
-  if (menu1_is_open) {
+  if (menu1IsOpen) {
     if (number == 1) {
       arrow1.src = "img/Arrow_Right.png"
       projektOneContainer.style.display = "none";
-      menu1_is_open = false;
+      menu1IsOpen  = false;
     }
   } else {
     if (number == 1) {
       arrow1.src = "img/Arrow_Down.png"
       projektOneContainer.style.display = "block";
-      menu1_is_open = true;
+      menu1IsOpen = true;
     }
   }
 
   const arrow2 = document.getElementById("projekt_two_arrow");
   const projektTwoContainer = document.getElementById("projekt_two_container");
-  if (menu2_is_open) {
+  if (menu2IsOpen) {
     if (number == 2) {
       arrow2.src = "img/Arrow_Right.png";
       projektTwoContainer.style.display = "none";
-      menu2_is_open = false;
+      menu2IsOpen = false;
     }
   } else {
     if (number == 2) {
       arrow2.src = "img/Arrow_Down.png";
       projektTwoContainer.style.display = "block";
-      menu2_is_open = true;
+      menu2IsOpen = true;
     }
   }
 
   const arrow3 = document.getElementById("projekt_three_arrow");
   const projektThreeContainer = document.getElementById("projekt_three_container");
-  if (menu3_is_open) {
+  if (menu3IsOpen) {
     if (number == 3) {
       arrow3.src = "img/Arrow_Right.png";
       projektThreeContainer.style.display = "none";
-      menu3_is_open = false;
+      menu3IsOpen = false;
     }
   } else {
     if (number == 3) {
       arrow3.src = "img/Arrow_Down.png";;
       projektThreeContainer.style.display = "block";
-      menu3_is_open = true;
+      menu3IsOpen = true;
     }
   }
 }
